f3d59e8aab7ea00a62dfd1ed0404598c6c3612cd
wuyinjie <1518986465@qq.com>
2017-01-25 09:23:34 +0800

    添加编译脚本

--- cocos2d-x-3.4/tools/cocos2d-console/plugins/plugin_luacompile/__init__.py
+++ cocos2d-x-3.4/tools/cocos2d-console/plugins/plugin_luacompile/__init__.py
@@ -21,6 +21,7 @@ import inspect
 import shutil
 
 import cocos
+from MultiLanguage import MultiLanguage
 
 ############################################################ 
 #http://www.coolcode.org/archives/?article-307.html
@@ -104,7 +105,7 @@ class CCPluginLuaCompile(cocos.CCPlugin):
     @staticmethod
     def brief_description():
         # returns a short description of this module
-        return "minifies and/or compiles lua files"
+        return MultiLanguage.get_string('LUACOMPILE_BRIEF')
 
     # This is not the constructor, just an initializator
     def init(self, options, workingdir):
@@ -123,11 +124,14 @@ class CCPluginLuaCompile(cocos.CCPlugin):
         self._isEncrypt = options.encrypt
         self._encryptkey = options.encryptkey
         self._encryptsign = options.encryptsign
+        self._bytecode_64bit = options.bytecode_64bit
+
         self._luajit_exe_path = self.get_luajit_path()
         self._disable_compile = options.disable_compile
 
         if self._luajit_exe_path is None:
-            raise cocos.CCPluginError("Can't find right luajit for current system.")
+            raise cocos.CCPluginError(MultiLanguage.get_string('LUACOMPILE_ERROR_TOOL_NOT_FOUND'),
+                                      cocos.CCPluginError.ERROR_TOOLS_NOT_FOUND)
 
         self._luajit_dir = os.path.dirname(self._luajit_exe_path)
 
@@ -143,11 +147,13 @@ class CCPluginLuaCompile(cocos.CCPlugin):
         try:
             pos = luafile.index(self._current_src_dir)
             if pos != 0:
-                raise cocos.CCPluginError("cannot find src directory in file path.")
+                raise cocos.CCPluginError(MultiLanguage.get_string('LUACOMPILE_ERROR_SRCDIR_NAME_NOT_FOUND'),
+                                          cocos.CCPluginError.ERROR_WRONG_ARGS)
 
             return luafile[len(self._current_src_dir)+1:]
         except ValueError:
-            raise cocos.CCPluginError("cannot find src directory in file path.") 
+            raise cocos.CCPluginError(MultiLanguage.get_string('LUACOMPILE_ERROR_SRCDIR_NAME_NOT_FOUND'),
+                                      cocos.CCPluginError.ERROR_WRONG_ARGS)
 
     def get_output_file_path(self, luafile):
         """
@@ -166,27 +172,32 @@ class CCPluginLuaCompile(cocos.CCPlugin):
         except OSError:
             if os.path.exists(dst_rootpath) == False:
                 # There was an error on creation, so make sure we know about it
-                raise cocos.CCPluginError("Error: cannot create folder in "+dst_rootpath)
+                raise cocos.CCPluginError(MultiLanguage.get_string('LUACOMPILE_ERROR_MKDIR_FAILED_FMT',
+                                                                   dst_rootpath),
+                                          cocos.CCPluginError.ERROR_PATH_NOT_FOUND)
 
         # print "return luac path: "+luac_filepath
         return luac_filepath
 
     def get_luajit_path(self):
         ret = None
-        if cocos.os_is_win32():
-            ret = os.path.join(self._workingdir, "bin", "luajit.exe")
-        elif cocos.os_is_mac():
-            ret = os.path.join(self._workingdir, "bin", "lua", "luajit-mac")
-        elif cocos.os_is_linux():
-            ret = os.path.join(self._workingdir, "bin", "lua", "luajit-linux")
 
+        bit_prefix = "64bit" if self._bytecode_64bit else "32bit"
+        if cocos.os_is_win32():
+            ret = os.path.join(self._workingdir, "bin", bit_prefix, "luajit-win32.exe")
+        elif cocos.os_is_mac():
+            ret = os.path.join(self._workingdir, "bin", bit_prefix, "luajit-mac")
+        elif cocos.os_is_linux():
+            ret = os.path.join(self._workingdir, "bin", bit_prefix, "luajit-linux")
+
+        print("luajit bin path: " + ret)
         return ret
 
     def compile_lua(self, lua_file, output_file):
         """
         Compiles lua file
         """
-        cocos.Logging.debug("compiling lua (%s) to bytecode..." % lua_file)
+        cocos.Logging.debug(MultiLanguage.get_string('LUACOMPILE_DEBUG_COMPILE_FILE_FMT', lua_file))
 
         with cocos.pushd(self._luajit_dir):
             cmd_str = "\"%s\" -b \"%s\" \"%s\"" % (self._luajit_exe_path, lua_file, output_file)
@@ -216,7 +227,7 @@ class CCPluginLuaCompile(cocos.CCPlugin):
         - `self`:
         """
 
-        cocos.Logging.info("processing lua script files")
+        cocos.Logging.info(MultiLanguage.get_string('LUACOMPILE_INFO_PROCESS_FILE'))
         index = 0
         for src_dir in self._src_dir_arr:
             for lua_file in self._lua_files[src_dir]:
@@ -243,12 +254,16 @@ class CCPluginLuaCompile(cocos.CCPlugin):
         """
         self.parse_args(argv)
 
+        # tips
+        cocos.Logging.warning(MultiLanguage.get_string('LUACOMPILE_WARNING_TIP_MSG'))
         # create output directory
         try:
             os.makedirs(self._dst_dir)
         except OSError:
             if os.path.exists(self._dst_dir) == False:
-                raise cocos.CCPluginError("Error: cannot create folder in " + self._dst_dir)
+                raise cocos.CCPluginError(MultiLanguage.get_string('LUACOMPILE_ERROR_MKDIR_FAILED_FMT',
+                                                                   self._dst_dir),
+                                          cocos.CCPluginError.ERROR_PATH_NOT_FOUND)
 
         # deep iterate the src directory
         for src_dir in self._src_dir_arr:
@@ -258,52 +273,60 @@ class CCPluginLuaCompile(cocos.CCPlugin):
 
         self.handle_all_lua_files()
 
-        cocos.Logging.info("compilation finished")
+        cocos.Logging.info(MultiLanguage.get_string('LUACOMPILE_INFO_FINISHED'))
 
     def parse_args(self, argv):
         """
         """
 
-        from optparse import OptionParser
+        from argparse import ArgumentParser
 
-        parser = OptionParser("usage: %prog luacompile -s src_dir -d dst_dir -v")
+        parser = ArgumentParser(prog="cocos %s" % self.__class__.plugin_name(),
+                                description=self.__class__.brief_description())
 
-        parser.add_option("-v", "--verbose",
+        parser.add_argument("-v", "--verbose",
                           action="store_true",
                           dest="verbose",
-                          help="verbose output")
-        parser.add_option("-s", "--src",
-                          action="append", type="string", dest="src_dir_arr",
-                          help="source directory of lua files needed to be compiled, supports mutiple source directory")
-        parser.add_option("-d", "--dst",
-                          action="store", type="string", dest="dst_dir",
-                          help="destination directory of lua bytecode files to be stored")
-        parser.add_option("-e", "--encrypt",
+                          help=MultiLanguage.get_string('LUACOMPILE_ARG_VERBOSE'))
+        parser.add_argument("-s", "--src", dest="src_dir_arr",
+                          action="append", help=MultiLanguage.get_string('LUACOMPILE_ARG_SRC'))
+        parser.add_argument("-d", "--dst", dest="dst_dir",
+                          help=MultiLanguage.get_string('LUACOMPILE_ARG_DST'))
+        parser.add_argument("-e", "--encrypt",
                           action="store_true", dest="encrypt",default=False,
-                          help="Whether or not to encrypt")
-        parser.add_option("-k", "--encryptkey",
+                          help=MultiLanguage.get_string('LUACOMPILE_ARG_ENCRYPT'))
+        parser.add_argument("-k", "--encryptkey",
                           dest="encryptkey",default="2dxLua",
-                          help="encrypt key")
-        parser.add_option("-b", "--encryptsign",
+                          help=MultiLanguage.get_string('LUACOMPILE_ARG_ENCRYPT_KEY'))
+        parser.add_argument("-b", "--encryptsign",
                           dest="encryptsign",default="XXTEA",
-                          help="encrypt sign")
-        parser.add_option("--disable-compile",
+                          help=MultiLanguage.get_string('LUACOMPILE_ARG_ENCRYPT_SIGN'))
+        parser.add_argument("--disable-compile",
                           action="store_true", dest="disable_compile", default=False,
-                          help="Whether or not to compile")
+                          help=MultiLanguage.get_string('LUACOMPILE_ARG_DISABLE_COMPILE'))
+        parser.add_argument("--bytecode-64bit",
+                          action="store_true", dest="bytecode_64bit", default=False,
+                          help=MultiLanguage.get_string('LUACOMPILE_ARG_BYTECODE_64BIT'))
 
-        (options, args) = parser.parse_args(argv)
+        options = parser.parse_args(argv)
 
         if options.src_dir_arr == None:
-            raise cocos.CCPluginError("Please set source folder by \"-s\" or \"-src\", run ./luacompile.py -h for the usage ")
+            raise cocos.CCPluginError(MultiLanguage.get_string('LUACOMPILE_ERROR_SRC_NOT_SPECIFIED'),
+                                      cocos.CCPluginError.ERROR_WRONG_ARGS)
         elif options.dst_dir == None:
-            raise cocos.CCPluginError("Please set destination folder by \"-d\" or \"-dst\", run ./luacompile.py -h for the usage ")
+            raise cocos.CCPluginError(MultiLanguage.get_string('LUACOMPILE_ERROR_DST_NOT_SPECIFIED'),
+                                      cocos.CCPluginError.ERROR_WRONG_ARGS)
         else:
             for src_dir in options.src_dir_arr:
                 if os.path.exists(src_dir) == False:
-                    raise cocos.CCPluginError("Error: dir (%s) doesn't exist..." % (src_dir))
+                    raise cocos.CCPluginError(MultiLanguage.get_string('LUACOMPILE_ERROR_DIR_NOT_EXISTED_FMT')
+                                              % (src_dir), cocos.CCPluginError.ERROR_PATH_NOT_FOUND)
 
         # script directory
-        workingdir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+        if getattr(sys, 'frozen', None):
+            workingdir = os.path.realpath(os.path.dirname(sys.executable))
+        else:
+            workingdir = os.path.realpath(os.path.dirname(__file__))
 
         self.init(options, workingdir)
 
